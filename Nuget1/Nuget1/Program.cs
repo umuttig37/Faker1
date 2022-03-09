7fed8833c400f9fb2586b87bb9bd731f983b8045
umuttig37 <umut.uygur30@gmail.com>
2022-03-09 10:26:27 +0200

    Import Faker 1 Project

---
+++ Nuget1/Nuget1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Nuget1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Person-----------------------------------------");
+            Console.WriteLine(Faker.Name.FullName());
+            Console.WriteLine(Faker.Country.Name());
+            Console.WriteLine(Faker.Address.StreetName());
+            Console.WriteLine(Faker.Identification.UsPassportNumber());
+            Console.WriteLine(Faker.Phone.Number());
+            Console.WriteLine(Faker.Company.Name());
+
+            Console.WriteLine("\nSecond Person----------------------------------------");
+            Console.WriteLine(Faker.Name.FullName());
+            Console.WriteLine(Faker.Country.Name());
+            Console.WriteLine(Faker.Address.StreetName());
+            Console.WriteLine(Faker.Identification.UsPassportNumber());
+            Console.WriteLine(Faker.Phone.Number());
+            Console.WriteLine(Faker.Internet.Email());
+
+            Console.WriteLine("\nThird Person------------------------------------------");
+            Console.WriteLine(Faker.Name.FullName());
+            Console.WriteLine(Faker.Country.Name());
+            Console.WriteLine(Faker.Address.StreetName());
+            Console.WriteLine(Faker.Lorem.Sentence());
+            Console.WriteLine(Faker.Phone.Number());
+            Console.WriteLine(Faker.Internet.Email());
+            Console.WriteLine(Faker.Internet.UserName());
+
+
+
+
+
+        }
+    }
+}
